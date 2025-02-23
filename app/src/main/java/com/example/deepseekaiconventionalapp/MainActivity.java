cf498aa1bd50c6cd9ea13c05a4db42a53d70c0ab
Sid <siddharthmethiya180@gmail.com>
2025-02-23 23:03:03 +0530

    Init all dependencies

--- app/src/main/java/com/example/deepseekaiconventionalapp/MainActivity.java
+++ app/src/main/java/com/example/deepseekaiconventionalapp/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.deepseekaiconventionalapp;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int RC_SIGN_IN = 9001;
-    private GoogleSignInClient signInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
